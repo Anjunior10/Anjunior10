5003be74a3e46fe2b3f3ece5fbaa382982cf27aa
Anjunior10 <anjunior311@gmail.com>
2024-12-15 00:27:28 -0300

    Add files via upload

---
+++ A.js
@@ -0,0 +1,51 @@
+
+  //----------botao1----------//
+  
+  function mudar(){
+    let btn = document.getElementById("btn"); 
+  let texto = document.getElementById("mudarUm");
+  if(btn.value=="ativo"){
+ document.getElementById("mudarUm").innerHTML= "Tudo começou quando me apaixonei.pela aquela menina sentanda sozinha...mas meu amigo disse q estava namorando com ela, e claro eu fui tirar pergunta kkkk mas era mentira...mas a partir daí começou!";
+   btn.value = "desligado"
+  }else{
+    texto.innerHTML=""
+    btn.value="ativo"
+  }
+}
+      //-----------botao2--------//
+function mudardois(){
+    let btn = document.getElementById("btndois"); 
+  let texto = document.getElementById("mudardois");
+  if(btn.value=="ativo"){
+ document.getElementById("mudardois").innerHTML= "No nosso primeiro encontro chegar o coraçao palpitava e a angustia batia(tbm me fizeram usar camisa combinando, mais foi por um bkm motivo kkkkk) entao chegamos la no acai conversamos, nada de muito pesado, tudo muito leve foi um dos nossos melhores pq tbm foi nosso primeiro td muito lindo rs";
+   btn.value = "desligado"
+  }else{
+    texto.innerHTML=""
+    btn.value="ativo"
+  }
+}
+      //-----------botao 3--------//
+function mudartres(){
+    let btn = document.getElementById("btntres"); 
+  let texto = document.getElementById("mudartres");
+  if(btn.value=="ativo"){
+ document.getElementById("mudartres").innerHTML= "Depois disso tivemos varios altos e baixos n foi facil chegar ate o noivado e engracado que nosso dia especial era o mesmo dia do aniversario de minha sogra e tive q inventar uma historia para conseguir tirar ela do aniversario e ao mesmo tempo ser rapido para nao fazer ela se atrasar pro trabalho...antes de pedir eu coloquei o buquê em um saco e pedir para ela segurar sem ela saber oque tinha dentro...e fomos ate o lugar e lá fiz o pedido noite maravilhosa kkkk ela ficou sem acreditar um tempo que tava noiva rs";
+   btn.value = "desligado"
+  }else{
+    texto.innerHTML=""
+    btn.value="ativo"
+  }
+}
+      //-----------botao 4--------//
+  function mudarquatro(){
+    let btn = document.getElementById("btnquatro"); 
+  let texto = document.getElementById("mudarquatro");
+  if(btn.value=="ativo"){
+ document.getElementById("mudarquatro").innerHTML= "E chegamos nos finalmentes kkkk pense em uma semana q deu tudo errado foi a semana do casamento mais graças a Deus deu tudo certo e em nome de jesus vai continuar dando certo...amém.";
+   btn.value = "desligado"
+  }else{
+    texto.innerHTML=""
+    btn.value="ativo"
+  }
+}
+      //-----------fim--------//
